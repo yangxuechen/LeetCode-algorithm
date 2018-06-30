68264d28a5d8f54f7d5d556d3e331f1737b7ab81
yangxuechen <1324688478@qq.com>
2018-06-30 10:02:54 +0800

    LeetCode292.Nim游戏

---
+++ LeetCode1/LeetCode292.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+public class LeetCode292 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public boolean canWinNim(int n) {
+	       if(n%4==0) {
+	    	   return false;
+	       }
+	       return true;
+	}
+}
