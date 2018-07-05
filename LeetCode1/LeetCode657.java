d9076edbaa10a23985d01b6bb9b533285b0c339e
yangxuechen <1324688478@qq.com>
2018-07-05 16:39:05 +0800

    LeetCode657.判断路线成圈

---
+++ LeetCode1/LeetCode657.java
@@ -0,0 +1,33 @@
+package leetCode2;
+
+public class LeetCode657 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="RLUDD";
+        boolean b=judgeCircle(str);
+        if(b) {
+        	System.out.println("true");
+        }else {
+        	System.out.println("false");
+        }
+	}
+    public static boolean judgeCircle(String moves) {
+        int x=0,y=0;   //定义坐标位置
+        for(char c:moves.toCharArray()) {
+        	if(c=='R') {
+        		y++;
+        	}else if(c=='L') {
+        		y--;
+        	}else if(c=='U') {
+        	    x++;
+        	}else if(c=='D') {
+        		x--;
+        	}
+        }
+        if(x==0&&y==0) {
+        	return true;
+        }
+    	return false;
+    }
+}
